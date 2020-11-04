9ae73ba3cb1bdc69fa44e2c4e0420099951165d0
vybhavP <podalavybhav@gmail.com>
2020-11-04 22:40:05 +0530

    adding material ui libraries

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders commands text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/commands/i);
   expect(linkElement).toBeInTheDocument();
 });
